1090a42af047d02f11b20bfaa0c67715d7159836
baki <tuncerbaki@gmail.com>
2024-05-30 21:29:09 +0100

    eread me file is editted

--- Back-End-Node/routes/cities.js
+++ Back-End-Node/routes/cities.js
@@ -3,6 +3,6 @@ const verifyToken = require("../middleware/verifyToken")
 const router = express.Router();
 const {cities} = require("../controllers/cities")
 
-router.get("/cities", cities)
+router.get("/cities",verifyToken, cities)
 
 module.exports = router;
--- Back-End-Node/routes/roles.js
+++ Back-End-Node/routes/roles.js
@@ -3,7 +3,7 @@ const verifyToken = require("../middleware/verifyToken")
 const router = express.Router();
 const {roles} = require("../controllers/roles")
 
-router.get("/roles", roles)
+router.get("/roles", verifyToken,roles)
 
 
 module.exports = router;
